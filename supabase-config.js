b200c919434731d081a798558e71824fabf548aa
LordSilkendor <pladsenschjelderup@gmail.com>
2026-07-15 22:22:55 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-window.LORDS_SUPABASE = {
-  url: 'https://ijhcesnidcnsxwqvtomr.supabase.co',
-  publishableKey: 'sb_publishable_gYYKk4iMjAAp57hEJ7VS1A_8Zs0Ae07',
-  siteUrl: 'https://lordsilkendor.github.io/LordKandan/'
-};
